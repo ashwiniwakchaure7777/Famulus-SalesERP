fa86585d14b802f5af3418a5c4e5d5129f1b8cc5
Ashwini <ashwiniwakchaure808@gmail.com>
2026-06-17 18:29:43 +0530

    env updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8056';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://famulus-saleserp.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
